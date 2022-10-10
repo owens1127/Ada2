be1b42b360d0ce7ea2472122c5481e0294b2410a
owensimpson <owensimpson1127@gmail.com>
2022-10-10 13:41:21 -0400

    deploy

--- deploy-commands.js
+++ deploy-commands.js
@@ -20,7 +20,7 @@ const rest = new REST({version: '10'}).setToken(token);
         console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
         const data = await rest.put(
-            Routes.applicationGuildCommands(clientId, guildId),
+            Routes.applicationCommands(clientId),
             {body: commands}
         );
 
