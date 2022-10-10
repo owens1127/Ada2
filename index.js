c99a3dbbbd4029eafdd3ede3e7b86840ec1c9325
owensimpson <owensimpson1127@gmail.com>
2022-10-10 12:22:12 -0400

    fixed index.js

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 // Require the necessary discord.js classes
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, Collection} = require('discord.js');
+const path = require('node:path');
+const fs = require('node:fs');
 const token = process.env.TOKEN
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-    console.log('Ready!');
-});
+// Commands
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, './commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection
+    // With the key as the command name and the value as the exported module
+    client.commands.set(command.data.name, command);
+}
+
+// Events
+const eventsPath = path.join(__dirname, './events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
 
 // Login to Discord with your client's token
 client.login(token);
