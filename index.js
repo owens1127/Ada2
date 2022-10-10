7120fe025008fc9ad0737dd3aba016880bd90bf5
owensimpson <owensimpson1127@gmail.com>
2022-10-10 12:18:08 -0400

    fixed env error

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Require the necessary discord.js classes
 const { Client, GatewayIntentBits } = require('discord.js');
-const token = process.env.DISCORD_TOKEN
+const token = process.env.TOKEN
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
