373fbd5a0efa713c2110464168baba46cc531022
owensimpson <owensimpson1127@gmail.com>
2022-11-13 23:20:11 -0500

    Token caching

--- bungie-net-api/main.mjs
+++ bungie-net-api/main.mjs
@@ -1,6 +1,7 @@
+import * as fs from 'fs';
 import { BungieClient, configure } from 'oodestiny';
 import { getAccessTokenFromRefreshToken } from 'oodestiny/util/tokens.js';
-import config from '../config.json' assert { type: 'json' }
+import tokens from '../tokens.json' assert { type: 'json' }
 
 /**
  * @type {BungieClient}
@@ -8,7 +9,19 @@ import config from '../config.json' assert { type: 'json' }
 export const client = await (async () => {
     configure(process.env.BUNGIE_NET_API_KEY, process.env.BUNGIE_NET_CLIENT_ID,
         process.env.BUNGIE_NET_SECRET);
-    const tokens = await getAccessTokenFromRefreshToken(config.refresh);
-    return new BungieClient(tokens.access.value);
+    const newTokens = await getAccessTokenFromRefreshToken(tokens.refresh.value);
+    fs.writeFileSync('./tokens.json', JSON.stringify(newTokens, null, 2))
+    const bc = new BungieClient(newTokens.access.value);
+    setInterval(refreshAccessToken, 3000000, bc);
+    return bc;
 })();
+
+function refreshAccessToken(bc) {
+    /** @type BungieNetTokens */
+    const readTokens = JSON.parse(fs.readFileSync('./tokens.json'));
+    getAccessTokenFromRefreshToken(readTokens.refresh.value).then(newTokens => {
+        fs.writeFileSync('./tokens.json', JSON.stringify(newTokens, null, 2))
+        bc.login(newTokens.access.value);
+    });
+}
 
