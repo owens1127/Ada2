8dcbdf92f3d4c9535fb943e6ec2a78c80f72d8d7
owensimpson <owensimpson1127@gmail.com>
2022-11-16 13:01:23 -0500

    added better command logging

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -2,13 +2,16 @@ module.exports = {
     name: 'interactionCreate',
     async execute(interaction) {
         console.log(
-            `${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
+            `${interaction.user.tag} in ${interaction.guild.name} #${interaction.channel.name} triggered an interaction.`);
 
         if (!interaction.isChatInputCommand()) return;
 
         const command = interaction.client.commands.get(interaction.commandName);
 
         if (!command) return;
+        console.log(
+            `${interaction.user.tag} used /${interaction.commandName} with params:`);
+        console.log(interaction.options._hoistedOptions)
 
         try {
             await command.execute(interaction);
