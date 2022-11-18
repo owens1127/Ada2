b6417b21c640fb7f5978b2d87ea3756367a5c41d
owensimpson <owensimpson1127@gmail.com>
2022-11-17 19:14:57 -0500

    adding manifest cache resetting and force fetching

--- bungie-net-api/manifest.mjs
+++ bungie-net-api/manifest.mjs
@@ -2,22 +2,31 @@ import { Manifest } from 'oodestiny';
 import { client } from './main.mjs';
 
 let destinyManifestCache;
-async function destinyManifest() {
-    destinyManifestCache = destinyManifestCache || await (async () => {
-            return client.Destiny2.GetDestinyManifest().then(r => r.Response);
-        })();
+
+async function destinyManifest(force) {
+    console.log('Fetching Destiny 2 Manifest');
+    destinyManifestCache = destinyManifestCache && !force ? destinyManifestCache : await (async () => {
+        setTimeout(clearManifestCache, 3 * 60 * 60000);
+        return client.Destiny2.GetDestinyManifest().then(r => r.Response);
+    })();
     return destinyManifestCache;
 }
+
+function clearManifestCache() {
+    console.log('Manifest cache cleared')
+    destinyManifestCache = null;
+}
+
 const cache = {};
 
 /**
  * Gets the DestinyInventoryItemDefinition in english
  * @return {Promise<{[p: number]: DestinyInventoryItemDefinition}>}
  */
-export async function getDestinyInventoryItemDefinitions() {
+export async function getDestinyInventoryItemDefinitions(force) {
     cache.DestinyInventoryItemDefinition =
         cache.DestinyInventoryItemDefinition || await Manifest.getDestinyManifestComponent({
-            destinyManifest: await destinyManifest(),
+            destinyManifest: await destinyManifest(force),
             tableName: Manifest.Components.DestinyInventoryItemDefinition,
             language: 'en'
         });
