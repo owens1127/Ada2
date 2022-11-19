c9d248a8fcc7fdabd9b3cf1c452bb4e6c5386ee3
owensimpson <owensimpson1127@gmail.com>
2022-11-19 16:12:25 -0500

    added permission checking for /channel

--- commands/channel.js
+++ commands/channel.js
@@ -1,4 +1,5 @@
-const { SlashCommandBuilder, ChannelType, TextChannel, PermissionFlagsBits } = require('discord.js');
+const { SlashCommandBuilder, ChannelType, TextChannel, PermissionFlagsBits } = require(
+    'discord.js');
 const { updateBroadcastChannel } = require('../database/guilds.js');
 module.exports = {
     data: new SlashCommandBuilder()
@@ -15,6 +16,12 @@ module.exports = {
         /** @type {TextChannel} */
         const channel = interaction.options.getChannel('channel');
         try {
+            //      PermissionFlagsBits.SendMessages = 0x800, 1 < 11, 2048
+            if (!(channel.permissionsFor(interaction.client.user).bitfield >> 11n & 1n)) {
+                return await interaction.editReply(
+                    { content: `Failed to update broadcast channel: bot is missing `
+                            + `permissions to send messages in ${channel.toString()}` });
+            }
             const updatedChannel = await updateBroadcastChannel(interaction.guild.id, channel);
             await interaction.editReply({
                 content: 'Broadcast channel successfully switched to '
