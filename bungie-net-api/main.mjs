26ad0cb5834c7e3eb39441fc5455be6fd75edd70
owensimpson <owensimpson1127@gmail.com>
2022-12-01 13:38:12 -0500

    no longer caching tokens

--- bungie-net-api/main.mjs
+++ bungie-net-api/main.mjs
@@ -1,11 +1,11 @@
 import * as fs from 'fs';
 import { BungieClient, configure, Tokens } from 'oodestiny';
-import tokens from '../local/tokens.json' assert { type: 'json' }
 
 /** @type {BungieClient} */
 export const client = await (async () => {
     configure(process.env.BUNGIE_NET_API_KEY, process.env.BUNGIE_NET_CLIENT_ID,
         process.env.BUNGIE_NET_SECRET);
+    const tokens = JSON.parse(fs.readFileSync('./local/tokens.json'));
     const newTokens = await Tokens.getAccessTokenFromRefreshToken(tokens.refresh.value);
     fs.writeFileSync('./local/tokens.json', JSON.stringify(newTokens, null, 2))
     const bc = new BungieClient(newTokens.access.value);
